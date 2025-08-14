17283cc44dc44f71b61741ed7f2a43a8c333c19a
Nicholas Bell <nick@advantagelearn.com>
2025-08-14 10:18:20 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\OrganisationController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
+
+Route::group(['middleware' => ['policy:viewInFrontend,Organisation']], function () {
+    Route::get('/', [OrganisationController::class, 'home'])->name('home');
+    Route::get('/overview/{year?}', [OrganisationController::class, 'overview'])->name('overview')->middleware('organisation.set');
+    Route::get('/priorities', [OrganisationController::class, 'priorities'])->name('priorities')->middleware('organisation.set');
+    Route::get('/calendar', [OrganisationController::class, 'calendar'])->name('calendar')->middleware('organisation.set');
+    Route::get('/indicators', [OrganisationController::class, 'indicators'])
+        ->name('indicators')
+        ->middleware(['organisation.set', 'throttle:10,1']);
+    Route::get('/inactive', [OrganisationController::class, 'inactive'])->name('inactive');
+    Route::get('/set', [OrganisationController::class, 'set'])->name('set');
+});
